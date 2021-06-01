1460b2e048f9fe84d47884a44e97d03fa0627019
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-05-31 22:16:41 -0300

    Page da Home

---
+++ bitcoin-rates/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormsLogin from '../Components/FormsLogin'
+
+function Home() {
+    return (
+        <div className='container-sm'>
+            <FormsLogin />
+        </div>
+    )
+}
+
+export default Home
