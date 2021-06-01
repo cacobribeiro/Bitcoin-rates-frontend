cb2805b786c6f7c525a50857eb9297eb6f193bff
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-05-31 22:15:34 -0300

    Header apenas visual

---
+++ bitcoin-rates/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <h2 className='m-auto'>Cotação Bitcoins</h2>
+    </nav>
+  );
+}
+
+export default Header;
